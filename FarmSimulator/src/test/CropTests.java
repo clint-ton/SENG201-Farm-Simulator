6bf88fce900dd73f2a6c433b7cb66f022f435b72
Clinton <incineratednewt@gmail.com>
2020-05-23 12:47:39 +1200

    crop tests

--- FarmSimulator/src/test/CropTests.java
+++ FarmSimulator/src/test/CropTests.java
@@ -15,19 +15,23 @@ import main.WeedSpray;
 import main.Crop;
 import main.CropItem;
 import main.Farm;
+import main.Farm1;
 import main.Farmer;
+import main.Farmer2;
  
 class CropTests {
    
     public Game setupTest() {
         Game game = new Game();
-        Farmer farmer = new Farmer("Skill", 0, 0);
-        Farm farm = new Farm("type", 0, 0, 9999);
+        Farmer player;
+        Farmer farmer = new Farmer2();
+        game.setPlayer(farmer, "name", 12);
+        Farm farm = new Farm1();
         game.setPlayerFarm(farm, "name");
         Rice rice = new Rice();
         WeedSpray item = new WeedSpray();
         game.purchaseProduct(rice, 0, 1);
-        game.purchaseProduct(item, 0, 99);
+        game.purchaseProduct(item, 0, 1);
         return game;
     }
    
@@ -41,30 +45,28 @@ class CropTests {
    
     @Test
     void tendToCrop() {
-    	int actions = 2;
-    	Game game = new Game();
+    	Game game = setupTest();
         Crop selectedCrop = game.getPlayerFarm().getCrops().get(0);
         CropItem selectedItem = (CropItem) game.getPlayerFarm().getItems().get(0);
     	game.tendToCrop(selectedItem, selectedCrop);
-    	assertEquals(game.getPlayerFarm().getCrops(),0);
-    	assertEquals(game.getPlayerFarm().getItems(), 0);
+    	assertEquals(game.getPlayerFarm().getCrops().get(0).getAgeDays(),1);
+    	assertTrue(game.getPlayerFarm().getItems().isEmpty());
+    	assertEquals(game.getActions(), 1);
     }
-//    @Test
-//    void test() {
-//        fail("Not yet implemented");
-//    }
-//    @Test
-//    void test() {
-//        fail("Not yet implemented");
-//    }
-//    @Test
-//    void test() {
-//        fail("Not yet implemented");
-//    }
-//    @Test
-//    void test() {
-//        fail("Not yet implemented");
-//    }
-//   
+    @Test
+    void harvestCrop() {
+        Game game = setupTest();
+        Crop selectedCrop = game.getPlayerFarm().getCrops().get(0);
+        selectedCrop.water();
+        System.out.println(selectedCrop.getSellPrice());
+        System.out.println(game.getPlayerFarm().getMoney());
+        System.out.println(game.harvestCrop(selectedCrop));
+        System.out.println(game.getPlayerFarm().getMoney());
+    	assertEquals(game.getActions(), 1);
+    	assertTrue(game.getPlayerFarm().getMoney() > 2000);
+    	assertTrue(game.getPlayerFarm().getCrops().isEmpty());
+
+    }
+
  
 }
